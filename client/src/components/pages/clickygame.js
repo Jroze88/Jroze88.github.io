71b4a397e914cc9b42ea84347202fe1d523a851a
Jroze88 <Jano.Roze@gmail.com>
2018-10-27 13:23:38 -0400

    card

--- client/src/components/pages/clickygame.js
+++ client/src/components/pages/clickygame.js
@@ -7,61 +7,223 @@ import $ from 'jquery';
 class ClickyGame extends Component {
 
 
+      state = {
+            score: 0
+      }
+
+      componentDidMount() {
+            $(document).ready(function() {
+
+            
+
+                  $('.stack').click(function() {
+        
+        $(".card").each(function(e) {
+      
+          setTimeout(function() {
+            $(".card").eq(e).attr("class", "card");
+          }, e * 150)
+          
+        });
+        
+      });
+      
+      $('.spread').click(function() {
+        
+        $(".card").each(function(e) {
+      
+          setTimeout(function() {
+            $(".card").eq(e).attr("class", "card ani" + e);
+          }, e * 150)
+          
+        });
+        
+      });
+      
+                  $('.flip-container').on('click', function() {
+
+                        
+                              
+                            
+                                  $(this).css("-webkit-transform","rotateY(180deg)");
+                                  $(this.firstChild).css("-webkit-transform","rotateY(180deg)");
+                               
+                                  $(this.firstChild.nextSibling).css("-webkit-transform","rotateY(180deg)");
+                              
+                        
+                     
+
+
+
+
+
+
+                  })
+      
+      
+      
+      
+      
+              })
+      }
+
 
 
     render() {
 
         return(
+            <div>
+                  <div className="announcement">
 
-            <div class="grid">
-        <ul class="list">
-          <li class="card ani1">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+                 Click a card to flip! 
+                  
+                  </div>
+                  <div className="scorediv">
+
+                  Score: {this.state.score}
+
+                  </div>
+
+
+            <div className="grid">
+
+        <ul className="list">
+          <li className="card ani1">
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani2">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani2">
+
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani3">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani3">
+  
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani4">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani4">
+  
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani5">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani5">
+      
+               <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani6">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani6">
+     
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
+            </li>
+          <li className="card ani7">
+      
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani7">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani8">
+      
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani8">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani0">
+        
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
-          <li class="card ani0">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
-          </li>
-          <li class="card ani9">
-                <a href="#"></a>
-                <span id="SPAN_3">stuff</span>
+          <li className="card ani9">
+         
+            <div className="flip-container">
+                 <div  className="flipper">
+		                  <div  className="front">
+                        front of card
+		                  </div>
+		                  <div  className="back">
+                        back of card
+		            </div>
+	            </div>
+                  </div>
           </li>
         </ul>
         
-        <button class="stack">Stack</button>
-        <button class="spread">Spread</button>
+        <button className="stack">Stack</button>
+        <button className="spread">Spread</button>
         
       </div>
-
+      </div>
 
 
         )
