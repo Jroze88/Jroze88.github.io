84450b3bcd53bae9a9407be8bad880abe33653b2
Jano Roze <39895226+Jroze88@users.noreply.github.com>
2019-01-24 10:41:15 -0500

    Update about.js

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -13,8 +13,8 @@ const About = props => (
     <p>
       I am a Laboratory Researcher and Full Stack Developer. As an impassioned
       problem solver I am equally comfortable drawing upon a wide [array] of my own experiences, employing the insights of my peers or
-      independently hacking away at new technologies to create practical solutons. A strong generalist; I retain a deep skillset
-      in a wide range of topics and 'learn on the go.' Please feel free to reach out by clicking the 'Contact' link in the navbar above!
+      independently hacking away at new technologies to create practical solutons. A strong generalist; I am comfortable working in a 
+       wide range of topics and 'learning on the fly.' Please feel free to reach out by clicking the 'Contact' link above!
     </p>
     <br></br>
     <br></br>
