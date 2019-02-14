fcbedca44a5a8b45ad9fc10c32d28d5517528869
Jroze88 <Jano.Roze@gmail.com>
2019-02-14 04:53:41 -0500

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,10 @@ module.exports = {
     ]
   },
   devServer: {
-    port: 3001,
+    port: 3000,
     open: true,
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'http://localhost:3001'
     }
   },
   plugins: [
