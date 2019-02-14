1fe43a4edcc4a1ea12fffec53edbda0ae6a7164c
Jroze88 <Jano.Roze@gmail.com>
2019-02-14 05:06:13 -0500

    build

---
+++ client/build/precache-manifest.1ae2ddf85eed80889b861e561bb5f0eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13ee30182f925c85711",
+    "url": "/static/css/main.8ea9569b.chunk.css"
+  },
+  {
+    "revision": "d13ee30182f925c85711",
+    "url": "/static/js/main.d13ee301.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cc0fd617b3ffad43dad0",
+    "url": "/static/js/2.cc0fd617.chunk.js"
+  },
+  {
+    "revision": "c287b5c24f66bc0d3896131e1516ecbf",
+    "url": "/static/media/border.c287b5c2.jpg"
+  },
+  {
+    "revision": "f19ee2e83acea2924c7990188efc5750",
+    "url": "/static/media/waxbutton.f19ee2e8.png"
+  },
+  {
+    "revision": "6355d169e78e85977d1a2b297015fdfa",
+    "url": "/static/media/crow.6355d169.png"
+  },
+  {
+    "revision": "126c4f2f3cbe0dd1c2955d3f5e4dd3d0",
+    "url": "/static/media/map.126c4f2f.jpg"
+  },
+  {
+    "revision": "392365a0e3e4f5704d45ba915566fb79",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV === "development") { // Configuration for development en
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, './client/src/index.js'));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
