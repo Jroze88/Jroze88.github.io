1223c3b5e02a271bdd36fa5c7080710fbfe656ce
Jroze88 <Jano.Roze@gmail.com>
2019-02-14 04:59:47 -0500

    heroku

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ if(process.env.NODE_ENV === "development") { // Configuration for development en
   const webpackCompiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(webpackCompiler));
   app.use(express.static(path.join(__dirname, "app")));
-} else if(process.env.NODE_ENV === "production") { // Configuration for production environment
-  app.use(express.static(path.join(__dirname, "dist")));
 }
 
 // Send every request to the React app
